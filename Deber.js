e93bade085824d8fb86de11a68ba19bfd6f54c71
sebs cuesta <kscuesta@sudamericano.edu.ec>
2023-12-17 20:04:01 -0500

    Initial commit

---
+++ Deber.js
@@ -0,0 +1,22 @@
+/*Partiendo del siguiente array, realizar las operaciones necesario para ordenar los días de la semana:
+
+let week = ['Suday', 'Tuesday', 'Wednesday', 'Thursday',  'Saturday' ]; 
+
+Eliminar el día domingo del inicio de la lista.
+Agregar el día lunes al inicio de la lista.
+Agregar el día domingo al final de la lista.
+Insertar el día viernes en la posición correspondiente.
+Mostrar en pantalla de elemento en elemento usando un for.
+Usar los métodos: push, pop, shift, unshift, splice */
+
+
+'use strict'
+let week = ['Suday', 'Tuesday', 'Wednesday', 'Thursday',  'Saturday' ]; 
+week.shift();
+week.unshift('Monday');
+week.push('Sunday');
+week.splice(4, 0, 'Friday');
+
+for (let i =0; i < week.length; i++) {
+    alert(week[i]);
+}
